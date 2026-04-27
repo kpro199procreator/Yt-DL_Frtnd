98983f0bb3073030026824d86ef62220d2433db6
Sucuboy-copy <outakun63@gmail.com>
2026-04-27 17:14:41 -0600

    Remove Theme of Androidmanifest.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,5 +87,6 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
